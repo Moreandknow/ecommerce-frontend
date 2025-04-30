a4e85e30cbfc7f9b0194740bee2b6a2042a7c9c3
Moreandknow <madrgame@gmail.com>
2025-04-30 23:55:55 +0700

    Finish Sidebar Dashboard

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# API List Made By MORENO ADRYAN CETA
+# Front End Progress List Made By MORENO ADRYAN CETA
 
 [x] Slicing Main Layout - Header
 [x] Slicing Main Layout - Footer
 [x] Slicing Main Secondary - Header
 [x] Fundamental Component - Slots
 [x] Slicing Layout Dashboard - Header
-[ ] Slicing Layout Dashboard - Sidebar
+[x] Slicing Layout Dashboard - Sidebar
--- layouts/Seller.vue
+++ layouts/Seller.vue
@@ -1,14 +1,53 @@
 <template>
   <div class="seller-layout">
     <LayoutsHeaderSeller />
-    <!-- <aside>Side Bar</aside> -->
+    <aside class="seller-sidebar">
+      <LayoutsSidebar :items="sidebarItems" />
+    </aside>
     <main class="main-content">
       <slot />
     </main>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const sidebarItems = [
+  {
+    label: "Pesanan",
+    icon: "i-heroicons:clipboard-document-list",
+    to: "/seller/order",
+  },
+  {
+    label: "Produk",
+    icon: "i-heroicons:inbox-stack",
+    children: [
+      {
+        label: "Produk Saya",
+        to: "/seller/product",
+      },
+      {
+        label: "Tambah Produk Baru",
+        to: "/seller/product/add",
+      },
+    ],
+  },
+  {
+    label: "Voucher",
+    icon: "i-heroicons:receipt-percent",
+    to: "/seller/voucher",
+  },
+  {
+    label: "Keuangan",
+    icon: "i-heroicons:banknotes",
+    children: [
+      {
+        label: "Saldo Saya",
+        to: "/seller/balance",
+      },
+    ],
+  },
+];
+</script>
 
 <style scoped>
 .seller-layout {
@@ -21,5 +60,13 @@
 .main-content {
   @apply flex-1;
   @apply bg-black/10;
+}
+
+.seller-sidebar {
+  @apply bg-white;
+  @apply p-4;
+  @apply fixed left-0 bottom-0;
+  top: 56px;
+  width: 240px;
 }
 </style>
