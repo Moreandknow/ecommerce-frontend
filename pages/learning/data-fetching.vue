37c4e1b887ead9ee8945c5940140bf84b40dc933
Moreandknow <madrgame@gmail.com>
2025-05-21 20:38:47 +0700

    learning useasyncdata

---
+++ pages/learning/data-fetching.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>Learning: Data Fetching</h3>
+    <UButton color="white" @click="execute">GET DATA</UButton>
+    <UButton @click="refresh">REFRESH DATA</UButton>
+    <p v-if="status === 'pending'">Loading...</p>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  middleware: ["learning-data-fetching"],
+});
+
+// const data = ref("");
+// const loading = ref(true);
+
+// onMounted(() => {
+//   fetch("https://dummyjson.com/products")
+//     .then((res) => res.json())
+//     .then((res) => {
+//       data.value = res;
+//     })
+//     .finally(() => (loading.value = false));
+// });
+
+function getProduct() {
+  return fetch("https://dummyjson.com/products").then((res) => res.json());
+}
+
+const { data, status, error, execute } = useAsyncData("product", getProduct, {
+  immediate: true,
+});
+
+function refresh() {
+  refreshNuxtData("product");
+}
+</script>
+
+<style lang="scss" scoped></style>
